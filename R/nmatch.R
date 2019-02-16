52076b4805d846ebec4568d09eba62490556b8bb
David Käthner <david.kaethner@dlr.de>
2019-02-16 11:43:50 +0100

    Finished nmatch and discovered that Hmisc had the same idea

--- R/nmatch.R
+++ R/nmatch.R
@@ -1,6 +1,11 @@
 #' Negative match
 #'
-#' While \code{match} returns the position of \code{x} where it was found in \code{y}, \code{nmatch} returns the position of \code{x} that was not found in \code{y}. For \code{%nix%}, the index of the non-intersecting values for \code{x} is returned.
+#' While \code{match} returns the position of \code{y} where an element of \code{x} was found, \code{nmatch} and \code{\%nin\%} returns the position of \code{x} which were not found in \code{y}. It is not possible to give the position of \code{y} where something was \emph{not} found.
+#'
+#' For \code{\%nix\%}, the index of the non-intersecting values for \code{x} is returned.
+#'
+#' @param x A vector
+#' @param table The data set to be queried.
 #'
 #' @return logical vector or index vector
 #'
@@ -12,7 +17,7 @@
 #' nmatch(x, y)
 #' x %nix% y
 #'
-#' @name
+#' @name nmatch
 NULL
 
 #' @rdname nmatch
